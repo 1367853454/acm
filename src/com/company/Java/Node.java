fe7c9f2b1e837ca2ca9930ad42f16c0caadf753e
ZHANGWJ <zhangwj_work@126.com>
2019-11-08 17:21:50 +0800

    'mm'

---
+++ src/com/company/Java/Node.java
@@ -0,0 +1,21 @@
+package com.company.Java;
+
+public class Node {
+
+    Node previous; //上一个节点
+    Node next;  //下一个节点
+    Object element;  //值
+
+    public Node(Node previous, Node next, Object element) {
+
+        super();
+        this.previous = previous;
+        this.next = next;
+        this.element = element;
+    }
+
+    public Node(Object element) {
+        super();
+        this.element = element;
+    }
+}
---
+++ src/com/company/Java/TestLinkedList.java
@@ -0,0 +1,75 @@
+package com.company.Java;
+
+public class TestLinkedList {
+
+    private Node first;
+
+    private Node last;
+
+    private int size;
+
+    public Object get(int index){
+
+        if (index < 0 || index > size - 1){
+            throw new RuntimeException("索引数字不合法" + index);
+        }
+
+        Node temp = null;
+
+        if (index <= (size >> 1)){
+            temp = first;
+            for (int i = 0; i < index; i++){
+                temp = temp.next;
+            }
+        }else {
+            temp = last;
+            for (int i = size -1; i > index; i--){
+                temp = temp.previous;
+            }
+        }
+        return temp.element;
+    }
+
+    public void add(Object obj){
+
+        Node node = new Node(obj);
+
+        if (null == first){
+            first = node;
+            last = node;
+        }else {
+            node.previous = last;
+            node.next = null;
+
+            last.next = node;
+            last = node;
+        }
+
+        size++;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("[");
+        Node temp = first;
+        while(null != temp){
+            stringBuilder.append(temp.element + ",");
+            temp = temp.next;
+        }
+        stringBuilder.setCharAt(stringBuilder.length()-1, ']');
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+
+        TestLinkedList linkedList = new TestLinkedList();
+
+        linkedList.add("a");
+        linkedList.add("b");
+        linkedList.add("c");
+
+        System.out.println(linkedList);
+        System.out.println(linkedList.get(2));
+    }
+
+}
